62395e836513ebcbc80bfee9b74dd17b99539ea7
George Williams <george.williams@gmail.com>
2020-05-01 14:43:04 -0700

    fix runcmds args

--- spacenet6/colab/bucket.py
+++ spacenet6/colab/bucket.py
@@ -1,7 +1,18 @@
 from google.colab import auth
 import os
 
+def checkjupyter(version=False):
+        print("Checking for jupyter/ipython environment...")
+        try:
+                from IPython import get_ipython
+                ipython = get_ipython()
+                print("Passed.")
+                return ipython
+        except:
+                return False
+
 def checkcolab():
+	print("Checking for colab environment...")
 	if not os.path.exists("/content"):
 		return False
 	else:
@@ -22,28 +33,33 @@ def runcmds(ipython, lines):
 
 def mount(bucket_name, force_new_mount=False):
 
+	ipython = checkjupyter()
+	if not ipython: raise Exception("This is not a jupyter environment.")
+
+	if not checkcolab(): raise Exception("This is not a colab environment.")
+
 	if force_new_mount and os.path.exists("/content/mountOnColab"):
-		runcmds( "fusermount -u mountOnColab" )
-		runcmds( "rmdir mountOnColab" )
+		runcmds( ipython, "fusermount -u mountOnColab" )
+		runcmds( ipython, "rmdir mountOnColab" )
 
 	if not os.path.exists("/content/mountOnColab"):
 		print("Trying to authenticate GCP user...")
 		auth.authenticate_user()
-		runcmds('echo "deb http://packages.cloud.google.com/apt gcsfuse-bionic main" > /etc/apt/sources.list.d/gcsfuse.list')
-		runcmds('curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - ' )
-		runcmds('apt -qq update')
-		runcmds('apt -qq install gcsfuse')
-		runcmds('mkdir mountOnColab')
-		runcmds("gcsfuse --implicit-dirs %s mountOnColab" % bucket_name )
+		runcmds( ipython, 'echo "deb http://packages.cloud.google.com/apt gcsfuse-bionic main" > /etc/apt/sources.list.d/gcsfuse.list')
+		runcmds( ipython, 'curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - ' )
+		runcmds( ipython, 'apt -qq update')
+		runcmds( ipython, 'apt -qq install gcsfuse')
+		runcmds( ipython, 'mkdir mountOnColab')
+		runcmds( ipython, "gcsfuse --implicit-dirs %s mountOnColab" % bucket_name )
 		print("Done. Getting folder contents...")
-		runcmds("ls -als /content/mountOnColab")
+		runcmds( ipython, "ls -als /content/mountOnColab")
 	else:
 		print("The locat folder mount exists.  Remounting remote system just in case...")
-		runcmds("fusermount -u mountOnColab")
-		runcmds("rmdir mountOnColab")
-		runcmds("mkdir mountOnColab")
-		runcmds("gcsfuse --implicit-dirs %s mountOnColab" % bucket_name)
+		runcmds( ipython, "fusermount -u mountOnColab")
+		runcmds( ipython, "rmdir mountOnColab")
+		runcmds( ipython, "mkdir mountOnColab")
+		runcmds( ipython, "gcsfuse --implicit-dirs %s mountOnColab" % bucket_name)
 		print("Done. Getting folder contents...")
-		runcmds("ls -als /content/mountOnColab")
+		runcmds(ipython,  "ls -als /content/mountOnColab")
 
 	return True
