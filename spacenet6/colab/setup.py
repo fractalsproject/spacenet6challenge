e98087cb297411ded47fa3e5fb03df46f25a2364
George Williams <george.williams@gmail.com>
2020-05-01 10:29:56 -0700

    fix _apt_get underscore mismatch

--- spacenet6/colab/setup.py
+++ spacenet6/colab/setup.py
@@ -1,12 +1,19 @@
 from IPython import get_ipython
 
-_aptget = "sudo apt-get install libgdal-dev libspatialindex-dev python-rtree gdal-bin build-essential"
+_apt_get = "sudo apt-get install libgdal-dev libspatialindex-dev python-rtree gdal-bin build-essential"
 
 def prereqs():
+	# verify its a notebook ( and version )
 	ipython = get_ipython()
-	#dir(ipython)
-	outp = ipython.system("echo 'Running system command: \"%s\"'.  Please wait..." % _aptget )
+	# TODO
+
+	# verify its colab ( and version )
+	# TODO
+
+	# do apt-get installs
+	outp = ipython.system("echo 'Running system command: \"%s\"'.  Please wait..." % _apt_get )
 	print(outp, type(outp))
 	outp = ipython.system( _apt_get )
 	print(outp, type(outp))
 
+	# do (external) pip installs
