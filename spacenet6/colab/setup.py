5b4ce7b8a251410bef0b2be2c58c70492f4c3990
George Williams <george.williams@gmail.com>
2020-05-01 10:20:01 -0700

    fix ipython syntax err

--- spacenet6/colab/setup.py
+++ spacenet6/colab/setup.py
@@ -3,7 +3,7 @@ from IPython import get_ipython
 _aptget = "sudo apt-get install libgdal-dev libspatialindex-dev python-rtree gdal-bin build-essential"
 
 def prereqs():
-	python = get_ipython()
+	ipython = get_ipython()
 	#dir(ipython)
 	outp = ipython.system("echo 'Running Command %s'" % _aptget )
 	print(outp)	
