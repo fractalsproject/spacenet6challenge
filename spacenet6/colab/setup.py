7bf4337cbc7be299ee963c9e13911eda98729ce8
George Williams <george.williams@gmail.com>
2020-05-01 12:46:13 -0700

    some 'refactor 'echo OK'

--- spacenet6/colab/setup.py
+++ spacenet6/colab/setup.py
@@ -1,32 +1,32 @@
 
 # These are the prereq commands that get individual invoked in the ipython session and checked.
 _prereq_cmds = '''\
-pip install affine>=2.3.0 && echo "OK affine"
-pip install albumentations==0.4.3 && echo "OK albumentations"
-pip install fiona>=1.7.13 && echo "OK fiona"
-pip install geopandas>=0.7.0 && echo "OK geopandas"
-pip install matplotlib>=3.1.2 && echo "OK matplotlib"
-pip install networkx>=2.4 && echo "OK networkx"
-pip install opencv-python>=4.1 && echo "OK opencv"
-pip install pandas>=0.25.3 && echo "OK pandas"
-pip install pyproj>=2.1 && echo "OK pyproj"
-pip install pyyaml==5.2 && echo "OK pyyaml"
-pip install rasterio>=1.0.23 && echo "OK rasterio"
-pip install requests==2.22.0 && echo "OK requests"
-pip install rtree>=0.9.3 && echo "OK rtree"
-pip install scikit-image>=0.16.2 && echo "OK scikit-image"
-pip install scipy>=1.3.2 && echo "OK scipy"
-pip install torchvision>=0.5.0 && echo "OK torchvision"
-pip install tqdm>=4.40.0 && echo "OK tqdm"
-pip install urllib3>=1.25.7 && echo "OK urllib3"
-sudo apt-get install -y software-properties-common && echo "OK"
-sudo add-apt-repository -y ppa:ubuntugis/ppa && apt-get -y update && echo "OK"
-sudo apt-get install -y libspatialindex-dev python-rtree gdal-bin && echo "OK"
-sudo apt-get install -y build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev && echo "OK"
-sudo apt-get install -y libgdal-dev && echo "OK"
-sudo gdal-config --version && echo "OK"
-pip install gdal>=3.0.2 && echo "OK gdal"
-cp /content/spacenet6challenge/solaris_setup_adj.py /content/spacenet6challenge/solaris/setup.py && cd /content/spacenet6challenge/solaris && python setup.py install && echo "OK"
+pip install affine>=2.3.0 
+pip install albumentations==0.4.3
+pip install fiona>=1.7.13 
+pip install geopandas>=0.7.0
+pip install matplotlib>=3.1.2
+pip install networkx>=2.4 
+pip install opencv-python>=4.1
+pip install pandas>=0.25.3 
+pip install pyproj>=2.1 
+pip install pyyaml==5.2
+pip install rasterio>=1.0.23
+pip install requests==2.22.0
+pip install rtree>=0.9.3 
+pip install scikit-image>=0.16.2
+pip install scipy>=1.3.2 
+pip install torchvision>=0.5.0
+pip install tqdm>=4.40.0 
+pip install urllib3>=1.25.7
+sudo apt-get install -y software-properties-common 
+sudo add-apt-repository -y ppa:ubuntugis/ppa && apt-get -y update
+sudo apt-get install -y libspatialindex-dev python-rtree gdal-bin
+sudo apt-get install -y build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev
+sudo apt-get install -y libgdal-dev 
+sudo gdal-config --version 
+pip install gdal>=3.0.2 
+cp /content/spacenet6challenge/solaris_setup_adj.py /content/spacenet6challenge/solaris/setup.py && cd /content/spacenet6challenge/solaris && python setup.py install
 '''
 
 def checksolaris(version=False):
@@ -61,7 +61,7 @@ def runcmds(ipython, lines):
 	cmds = [ cmd for cmd in lines.split("\n") if not cmd=="" ]
 	for cmd in cmds:
 		print( "Running command: \"%s\".  Please wait..." % cmd )
-		outputs = ipython.getoutput( cmd )
+		outputs = ipython.getoutput( cmd + ' && echo "OK."' )
 		#print(outputs)
 		if any( [ outp.startswith("OK") for outp in outputs ] ):
 			print("OK.")
