c92999a9f91f83196338019fb19ebfcd7492e33a
George Williams <george.williams@gmail.com>
2020-05-01 22:43:32 -0700

    fix runcmds

--- spacenet6/colab/setup.py
+++ spacenet6/colab/setup.py
@@ -35,6 +35,7 @@ cd /content/spacenet6challenge/solaris && pip install .
 
 def runcmds(ipython, lines, errcheck=True, showoutput=False):
 	cmds = [ cmd for cmd in lines.split("\n") if not cmd=="" ]
+	print("CMDS",cmds)
 	for cmd in cmds:
 
 		print( "Running command: \"%s\".  Please wait..." % cmd )
