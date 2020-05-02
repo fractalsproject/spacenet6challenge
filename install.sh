a94bc878dc357ecabc1c740f441cbe993cc46288
George Williams <george.williams@gmail.com>
2020-05-02 13:30:56 -0700

    project cleanup

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "spacenet6challenge" ]; 
-	then git clone --recursive "https://github.com/fractalsproject/spacenet6challenge.git" ; 
-else 
-	echo "spacenet6challenge directory already exists"; 
-fi
-
-
