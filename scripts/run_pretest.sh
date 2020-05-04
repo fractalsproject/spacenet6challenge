df689f4bfb2060a92d56d1db69bc36e2846ddfe1
George Williams <george.williams@gmail.com>
2020-05-04 00:16:17 +0000

    using correct path to data dir

--- scripts/run_pretest.sh
+++ scripts/run_pretest.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+set -e
+set -x
+
 INSTNAME="miniconda_for_spacenet6_local_jupyter"
+
+PATH_TO_TEST_DATA="/root/Projects/fractalsproject/data/test_public"
 
 if [ ! -d "$HOME/opt/$INSTNAME" ]; then
 	echo "This system is not configured with the right conda environment"
+	exit 1
+fi
+
+if [ ! -d "$PATH_TO_TEST_DATA" ]; then
+	echo "Could not find test data."
 	exit 1
 fi
 
@@ -16,12 +26,14 @@ which python
 source "$HOME/opt/$INSTNAME/etc/profile.d/conda.sh"
 conda activate base
 
-testdatapath="../data/test_public/AOI_11_Rotterdam/" $ $1
+testdatapath="$PATH_TO_TEST_DATA/AOI_11_Rotterdam/" # $1
 outputpath="./root" # $2
 testdataargs="\
 --testdir $testdatapath/SAR-Intensity \
 --outputcsv $outputpath \
 "
+
+mkdir -p "$outputpath"
 
 source scripts/settings.sh
 
