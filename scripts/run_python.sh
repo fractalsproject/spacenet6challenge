a8b86add08e8d89c9e22a1ea43f67d48f3c670ee
George Williams <george.williams@gmail.com>
2020-05-07 09:44:35 -0700

    checking for eval images already processed

--- scripts/run_python.sh
+++ scripts/run_python.sh
@@ -12,10 +12,10 @@ if [ ! -d "$HOME/opt/$INSTNAME" ]; then
 	exit 1
 fi
 
-if [ ! -d "$PATH_TO_TEST_DATA" ]; then
-	echo "Could not find test data."
-	exit 1
-fi
+#if [ ! -d "$PATH_TO_TEST_DATA" ]; then
+#	echo "Could not find test data."
+#	exit 1
+#fi
 
 export PATH="$HOME/opt/miniconda_for_spacenet6_local_jupyter/bin:$PATH"
 echo $PATH
@@ -26,4 +26,4 @@ which python
 source "$HOME/opt/$INSTNAME/etc/profile.d/conda.sh"
 conda activate base
 
-python
+PYTHONPATH='./solaris' python
